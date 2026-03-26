80ad7fdaf7246eae0c30f5440daa7a3ec318dc20
Saifalanii <saifalani1996@gmail.com>
2026-03-26 17:00:41 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -12,18 +12,13 @@
       "src": "icon.png",
       "sizes": "192x192",
       "type": "image/png",
-      "purpose": "any"
-    },
-    {
-      "src": "icon.png",
-      "sizes": "192x192",
-      "type": "image/png",
-      "purpose": "maskable"
+      "purpose": "any maskable"
     },
     {
       "src": "icon.png",
       "sizes": "512x512",
-      "type": "image/png"
+      "type": "image/png",
+      "purpose": "any maskable"
     }
   ]
 }
