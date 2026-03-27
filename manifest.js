343919d0c8612e7ff382cf9de1eff028ef7922a5
Saifalanii <saifalani1996@gmail.com>
2026-03-27 19:55:19 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -3,18 +3,18 @@
   "name": "Skiftrapport Samhall",
   "icons": [
   {
-    "src": "/icon.png",
+    "src": "icon.png",
     "sizes": "192x192",
     "type": "image/png"
   },
   {
-    "src": "/icon.png",
+    "src": "icon.png",
     "sizes": "512x512",
     "type": "image/png",
     "purpose": "any maskable"
     }
   ],
-  "start_url": ".",
+  "start_url": "./index.html",
   "display": "standalone",
   "theme-color": "#5b5fc7",
   "background_color": "#e8eaed"
