fc14727c7510a92fd93b7bfe716775fec2fce276
Saifalanii <saifalani1996@gmail.com>
2026-03-27 16:02:56 +0100

    Update manifest.js

--- manifest.js
+++ manifest.js
@@ -1,24 +1,21 @@
 {
-  "name": "Skiftrapport Volvo Samhall",
   "short_name": "Rapport",
-  "description": "Digital skiftrapport för TVS och TPB",
-  "start_url": "index.html",
-  "display": "standalone",
-  "orientation": "portrait",
-  "background_color": "#111111",
-  "theme_color": "#5b5fc7",
+  "name": "Skiftrapport Samhall",
   "icons": [
     {
       "src": "icon.png",
       "sizes": "192x192",
       "type": "image/png",
-      "purpose": "any"
+      "purpose": "any maskable"
     },
     {
       "src": "icon.png",
       "sizes": "512x512",
-      "type": "image/png",
-      "purpose": "any"
+      "type": "image/png"
     }
-  ]
+  ],
+  "start_url": ".",
+  "display": "standalone",
+  "theme-color": "#5b5fc7",
+  "background_color": "#e8eaed"
 }
